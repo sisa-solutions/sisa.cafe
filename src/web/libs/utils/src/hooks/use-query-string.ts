5035d75c9d57881b2889533c1d8c65ee7e4baeb8
Sang Au <sangau@sisa.io>
2023-09-26 11:28:26 +0700

    feat: draft use queryString
    Signed-off-by: Sang Au <sangau@sisa.io>

---
+++ src/web/libs/utils/src/hooks/use-query-string.ts
@@ -0,0 +1,51 @@
+import { useCallback, useEffect } from 'react';
+
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+
+const useQueryString = <QueryObject extends Record<string, string | number>>(
+  initParams?: QueryObject
+) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (initParams) {
+      const pararms = new URLSearchParams(searchParams);
+
+      Object.keys(initParams).forEach((key) => {
+        const value = initParams[key];
+
+        if (value) {
+          pararms.set(key, value.toString());
+        }
+      });
+
+      router.push(`${pathname}?${pararms.toString()}`);
+    }
+  }, [initParams]);
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const setQueryString = useCallback(
+    (newParams: Partial<QueryObject>) => {
+      const pararms = new URLSearchParams(searchParams);
+
+      Object.keys(newParams).forEach((key) => {
+        const value = newParams[key];
+
+        if (value) {
+          pararms.set(key, value.toString());
+        } else {
+          pararms.delete(key);
+        }
+      });
+
+      router.push(`${pathname}?${pararms.toString()}`);
+    },
+    [searchParams]
+  );
+
+  return [setQueryString] as const;
+};
+
+export default useQueryString;
