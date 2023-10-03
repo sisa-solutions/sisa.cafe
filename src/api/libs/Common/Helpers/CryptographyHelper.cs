b82a4774ae6008942a9ea3276c1353121c74febe
Sang Au <sangau@sisa.io>
2023-10-03 11:37:30 +0700

    chore: use .net 8 for all project, include source gen
    Signed-off-by: Sang Au <sangau@sisa.io>

--- src/api/libs/Common/Helpers/CryptographyHelper.cs
+++ src/api/libs/Common/Helpers/CryptographyHelper.cs
@@ -1,51 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Sisa.Common.Helpers
+namespace Sisa.Common.Helpers;
+
+public static class CryptographyHelper
 {
-    public static class CryptographyHelper
+    public static string AesEncrypt(string key, string plainText)
     {
-        public static string AesEncrypt(string key, string plainText)
+        byte[] iv = new byte[16];
+        byte[] array;
+
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = iv;
+
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+        using MemoryStream memoryStream = new();
+        using CryptoStream cryptoStream = new(memoryStream, encryptor, CryptoStreamMode.Write);
+        using (StreamWriter streamWriter = new(cryptoStream))
         {
-            byte[] iv = new byte[16];
-            byte[] array;
-
-            using Aes aes = Aes.Create();
-            aes.Key = Encoding.UTF8.GetBytes(key);
-            aes.IV = iv;
-
-            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-            using MemoryStream memoryStream = new();
-            using CryptoStream cryptoStream = new((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
-            using (StreamWriter streamWriter = new((Stream)cryptoStream))
-            {
-                streamWriter.Write(plainText);
-            }
-
-            array = memoryStream.ToArray();
-
-            return Convert.ToBase64String(array);
+            streamWriter.Write(plainText);
         }
 
-        public static string AesDecrypt(string key, string cipherText)
-        {
-            byte[] iv = new byte[16];
-            byte[] buffer = Convert.FromBase64String(cipherText);
+        array = memoryStream.ToArray();
 
-            using Aes aes = Aes.Create();
-            aes.Key = Encoding.UTF8.GetBytes(key);
-            aes.IV = iv;
-            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        return Convert.ToBase64String(array);
+    }
 
-            using MemoryStream memoryStream = new(buffer);
-            using CryptoStream cryptoStream = new((Stream)memoryStream, decryptor, CryptoStreamMode.Read);
-            using StreamReader streamReader = new((Stream)cryptoStream);
-            return streamReader.ReadToEnd();
-        }
+    public static string AesDecrypt(string key, string cipherText)
+    {
+        byte[] iv = new byte[16];
+        byte[] buffer = Convert.FromBase64String(cipherText);
+
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = iv;
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using MemoryStream memoryStream = new(buffer);
+        using CryptoStream cryptoStream = new(memoryStream, decryptor, CryptoStreamMode.Read);
+        using StreamReader streamReader = new(cryptoStream);
+
+        return streamReader.ReadToEnd();
     }
 }
