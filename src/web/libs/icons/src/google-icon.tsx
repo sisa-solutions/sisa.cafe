e97b41d57dd1e409fe8559c8ebd8aff92d9b4796
Sang Au <sangau@sisa.io>
2023-11-06 14:27:09 +0700

    chore: extra-icons
    Signed-off-by: Sang Au <sangau@sisa.io>

---
+++ src/web/libs/icons/src/google-icon.tsx
@@ -0,0 +1,23 @@
+import { createLucideIcon } from 'lucide-react';
+
+// Reference: https://tabler-icons.io/i/brand-google
+const GoogleIcon = createLucideIcon('Google', [
+  [
+    'path',
+    {
+      d: 'M0 0h24v24H0z',
+      stroke: 'none',
+      fill: 'none',
+      key: '1',
+    },
+  ],
+  [
+    'path',
+    {
+      d: 'M17.788 5.108a9 9 0 1 0 3.212 6.892h-8',
+      key: '2',
+    },
+  ],
+]);
+
+export default GoogleIcon;
